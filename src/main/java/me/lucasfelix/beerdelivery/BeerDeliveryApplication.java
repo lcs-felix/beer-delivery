8e4de767e788dae02368e8a2058a25e95bace260
Lucas Felix <lfelixsampaio@gmail.com>
2018-06-26 08:10:43 -0300

    inital commit

---
+++ src/main/java/me/lucasfelix/beerdelivery/BeerDeliveryApplication.java
@@ -0,0 +1,12 @@
+package me.lucasfelix.beerdelivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeerDeliveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeerDeliveryApplication.class, args);
+	}
+}
